79b8f38a65baa9ba41e87f16e577e0207d1bf6ca
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-02-28 13:26:41 +0000

    Avoid saving energy and its data as a single comma-separated string in one field

--- mantis_xray/data_stack.py
+++ mantis_xray/data_stack.py
@@ -709,7 +709,7 @@ class data:
                 return
             else:
                 for ie in range(self.n_ev):
-                    writer.writerow(['{0:06.2f}, {1:012g}'.format(evdata[ie], data[ie])])
+                    writer.writerow([f'{evdata[ie]:06.2f}', f'{data[ie]:012g}'])
                 return
 
     # ----------------------------------------------------------------------
